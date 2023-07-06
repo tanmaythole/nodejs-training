3910307560920d3157ec2ee482ebf19e040273a3
Tanmay Thole <tanmaythole@gmail.com>
2023-07-06 16:59:58 +0530

    integrated mysql

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+const db = require("./db");
+
 const app = express();
 
 // enable cors for all requests
@@ -9,6 +14,14 @@ app.use(cors())
 const args = process.argv.splice(2)
 const env = args[0];
 const port = args[1]?.split("=")[1]
+
+// connecting the db
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("DB connected!");
+})
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
