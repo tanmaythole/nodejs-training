de42628aace60fef3b49568381ea021b290f7614
Tanmay Thole <tanmaythole@gmail.com>
2023-07-06 11:35:20 +0530

    env port added from command line

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 
 const app = express();
 
+const args = process.argv.splice(2)
+const env = args[0];
+const port = args[1]?.split("=")[1]
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(5000, () => {
-    console.log("Server listening at port 5000");
+app.listen(port || 5000, () => {
+    console.log(`${env} server listening at port ${port}`);
 });
